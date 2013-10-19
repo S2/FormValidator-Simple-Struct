d9fa267e61edf3b52a1e8b0c7432733a8f9e0e64
sato sin <shinichiro@wano.co.jp>
2013-10-19 14:59:05 +0900

    first commit

---
+++ lib/FormValidator/Simple/Struct.pm
@@ -0,0 +1,39 @@
+package FormValidator::Simple::Struct;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+FormValidator::Simple::Struct - It's new $module
+
+=head1 SYNOPSIS
+
+    use FormValidator::Simple::Struct;
+
+=head1 DESCRIPTION
+
+FormValidator::Simple::Struct is ...
+
+=head1 LICENSE
+
+Copyright (C) sato sin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sato sin E<lt>shinichiro@wano.co.jpE<gt>
+
+=cut
+
